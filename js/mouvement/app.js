4afd855a477ceba05911a6f34f008aea55637809
josue bondo <josuebondojw@gmail.com>
2026-07-15 15:08:13 +0100

    dele js dir

--- js/mouvement/app.js
+++
@@ -1,9 +0,0 @@
-import { ApiService } from "./ApiService.js";
-
-const api = new ApiService("https://stock.skabi.shop/");
-
-let bons = [];
-let filteredBons = [];
-let currentPage = 1;
-let pageSize = 32; // Nombre de lignes par page
-
