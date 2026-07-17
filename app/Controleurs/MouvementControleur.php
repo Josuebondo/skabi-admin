067118eb39bf1c07c736424cec83122ae3dc5215
josue bondo <josuebondojw@gmail.com>
2026-07-17 16:39:12 +0100

    teste deplyoment

--- app/Controleurs/MouvementControleur.php
+++ app/Controleurs/MouvementControleur.php
@@ -8,6 +8,8 @@ class MouvementControleur extends BaseControleur
 {
     public function index()
     {
+
+        dd('teste deployement');
         $this->afficher('mouvement/index', [
             'titre' => 'Liste des mouvements',
             'mouvements' => [] // Vous pouvez remplacer ceci par une récupération réelle des mouvements depuis la base de données
